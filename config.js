d6b9fc46a135183ce7b8ede92afa18662012bc89
gpojarvis-sudo <gpojarvis@gmail.com>
2026-04-15 18:02:14 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+
+// 🔐 Hugging Face Token यहाँ डालो
+export const HF_TOKEN = "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";
+
+// 🤖 Model
+export const HF_MODEL = "google/flan-t5-large";
+
+// 🌐 Optional backend
+export const API_URL = "";
